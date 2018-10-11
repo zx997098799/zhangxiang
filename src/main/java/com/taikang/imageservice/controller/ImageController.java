7c200b327c409e404a407d60295f59af351efde9
zx997098799 <18235417441@163.com>
2018-10-11 10:47:23 +0800

    3期第二次修改

--- src/main/java/com/taikang/imageservice/controller/ImageController.java
+++ src/main/java/com/taikang/imageservice/controller/ImageController.java
@@ -55,6 +55,7 @@ public class ImageController {
         //TODO 2.根据content智能过滤获取科室信息
         System.out.println("3其改动，无需提交到master");
         System.out.println("2期改动，需合并");
+        System.out.println("3期的第二次修改");
         List<ClinicInfoBo> clinicInfoBoList=imageService.selectClinicInfo();
         return ResponseMessage.ok(clinicInfoBoList);
     }
