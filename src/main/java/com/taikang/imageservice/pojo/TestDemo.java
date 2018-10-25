117940feaa84a399adb447d15530fc9a878be2a3
zx997098799 <18235417441@163.com>
2018-10-25 20:03:55 +0800

    第一次提交

---
+++ src/main/java/com/taikang/imageservice/pojo/TestDemo.java
@@ -0,0 +1,7 @@
+package com.taikang.imageservice.pojo;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
--- src/main/java/com/taikang/imageservice/pojo/User.java
+++ src/main/java/com/taikang/imageservice/pojo/User.java
@@ -1,22 +1,25 @@
 package com.taikang.imageservice.pojo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private  Integer userId;
     private  String userName;
 
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
+   
+    public static void main(String[] args) {
+        List<String> a=new ArrayList<String>();
+        a.add("q");
+        List<String> b=new ArrayList<String>();
+        b.add("a");
+        b.addAll(a);
+        System.out.println(b.size());
+        String s="a";
+        String[] ss= s.split(",");
+        System.out.println(ss.length);
+        String s1="a,b";
+        String[] ss1= s1.split(",");
+        System.out.println(ss1.length);
     }
 }
