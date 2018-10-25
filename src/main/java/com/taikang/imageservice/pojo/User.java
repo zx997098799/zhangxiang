f028c91b3a7c8d4eba57427a8f90723a98a69d28
zx997098799 <18235417441@163.com>
2018-10-25 20:06:18 +0800

    啊实打实的

--- src/main/java/com/taikang/imageservice/pojo/User.java
+++ src/main/java/com/taikang/imageservice/pojo/User.java
@@ -7,7 +7,22 @@ public class User {
     private  Integer userId;
     private  String userName;
 
-   
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public static void main(String[] args) {
         List<String> a=new ArrayList<String>();
         a.add("q");
